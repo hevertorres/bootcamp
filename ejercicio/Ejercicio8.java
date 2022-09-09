fd38b290c7b1769bf3966d56837e5caf212ea9ae
Hever Torres D <hevertorres@gmail.com>
2022-09-08 22:24:17 -0500

    terminando Ejercicio 8

--- ejercicio/Ejercicio8.java
+++ ejercicio/Ejercicio8.java
@@ -13,7 +13,43 @@ public class Ejercicio8 {
         System.out.println("----  -----------  --------");
     }
 
-    
+    /* Ejercicios tema 8
+Para practicar la encapsulación:
+Crear clase Persona.
+Crear variables las privadas edad, nombre y telefono de la clase Persona.
+Crear gets y sets de cada propiedad.
+Crear un objeto persona en el main.
+Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, 
+por último muéstralas por consola.
+ */
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+  //   edad
+    public void setsEdad(int edad){
+      this.edad = edad;
+    }
+    public int getsEdad(){
+          return this.edad;
+    }
+  //   nombre
+    public void setsNombre(String nombre){
+      this.nombre=nombre;
+    }
+    public String getsNombre(){
+      return this.nombre;
+    }
+  //   telefono
+  public void setsTelefono(String telefono){
+      this.telefono=telefono;
+  
+  }
+  public String getsTelefono(){
+      return this.telefono;
+  }
+}  
+
 
 
 }
